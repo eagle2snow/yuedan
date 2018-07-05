e766e36a83689543d96f1533fec7f5f8f4ba9f11
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-07-05 07:29:17 +0000

    删除member 添加技能实体

---
+++ base/src/main/java/com/hhs/base/model/Skill.java
@@ -0,0 +1,67 @@
+package com.hhs.base.model;
+
+import com.hhs.gencode.annotation.FormField;
+import com.hhs.gencode.annotation.M;
+import com.hhs.gencode.util.FieldType;
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+/**
+ * @Auther: Eagle
+ * @Date: 2018/7/5 11:20
+ * @Description: 客户技能实体类
+ */
+@Data
+@M("技能表")
+@Entity(name = "skill")
+@Table(name = "t_skill")
+@SuppressWarnings("serial")
+public class Skill extends Model {
+
+    @FormField(type = FieldType.TEXTINPUT, label = "会员")
+    private Client client;
+
+    @FormField(type = FieldType.NUMBER, label = "分类")
+    private Integer classesId;
+
+    @FormField(type = FieldType.NUMBER, label = "咨询类型(品类)")
+    private Integer categoryId;
+
+    @FormField(type = FieldType.PICTURE, label = "技能相册")
+    private String skillImagesUrl;
+
+    @FormField(type = FieldType.RADIO, label = "咨询对象", data = "1|单方需要咨询,2|双方需要咨询")
+    private Integer statusAdvisory;
+
+    /**
+     * 1:线下服务 2:线上服务 3:电话咨询 4:视频咨询
+     */
+    @FormField(type = FieldType.TEXTINPUT, label = "服务类型")
+    private String type;
+
+    @FormField(type = FieldType.NUMBER, label = "服务价格")
+    private BigDecimal serviceMoney = BigDecimal.ZERO;
+
+    @FormField(type = FieldType.RADIO, label = "派单状态", data = "1|接受派单,2|不接派单")
+    private Integer statusSendOrder;
+
+    @FormField(type = FieldType.RADIO, label = "应邀数")
+    private Integer totalInvitation;
+
+    @FormField(type = FieldType.RADIO, label = "审核状态", data = "1|待完善，2|审核中,3|已驳回,4|已通过")
+    private Integer statusCheck;
+
+    @ManyToOne
+    public Client getClient() {
+        return client;
+    }
+
+    public void setMember(Client client) {
+        this.client = client;
+    }
+
+}
