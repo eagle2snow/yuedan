6c8eeae10368a579bc31d83253f0b310eaeb00e0
lyz <lyz@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-07-06 01:01:48 +0000

    分类管理

---
+++ base/src/main/java/com/hhs/base/dao/ClassifyDao.java
@@ -0,0 +1,10 @@
+package com.hhs.base.dao;
+
+import org.springframework.stereotype.Repository;
+import com.hhs.base.dao.BaseDao;
+import com.hhs.base.model.Classify;
+
+@Repository
+public class ClassifyDao extends BaseDao<Classify, Integer> {
+
+}
---
+++ base/src/main/java/com/hhs/base/dao/ServiceClassDao.java
@@ -0,0 +1,10 @@
+package com.hhs.base.dao;
+
+import org.springframework.stereotype.Repository;
+import com.hhs.base.dao.BaseDao;
+import com.hhs.base.model.ServiceClass;
+
+@Repository
+public class ServiceClassDao extends BaseDao<ServiceClass, Integer> {
+
+}
