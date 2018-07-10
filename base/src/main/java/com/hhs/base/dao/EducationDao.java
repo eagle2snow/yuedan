0fbfa425dd45a4b2689e660fbdffa40246295392
yy <yy@c9df1598-7c2d-47fe-a70c-8cfe019b57d4>
2018-07-10 01:03:04 +0000

    添加教育、工作dao

---
+++ base/src/main/java/com/hhs/base/dao/EducationDao.java
@@ -0,0 +1,10 @@
+package com.hhs.base.dao;
+
+import org.springframework.stereotype.Repository;
+import com.hhs.base.dao.BaseDao;
+import com.hhs.base.model.Education;
+
+@Repository
+public class EducationDao extends BaseDao<Education, Integer> {
+
+}
---
+++ base/src/main/java/com/hhs/base/dao/WorkDao.java
@@ -0,0 +1,10 @@
+package com.hhs.base.dao;
+
+import org.springframework.stereotype.Repository;
+import com.hhs.base.dao.BaseDao;
+import com.hhs.base.model.Work;
+
+@Repository
+public class WorkDao extends BaseDao<Work, Integer> {
+
+}
